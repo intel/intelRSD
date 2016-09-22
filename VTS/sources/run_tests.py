19de3b896cc900ffa6fcb4d7c9083aee7552e75e
Maciej Romanowski <maciej.a.romanowski@intel.com>
2016-09-22 22:27:29 +0200

    Added lost update

--- VTS/sources/run_tests.py
+++ VTS/sources/run_tests.py
@@ -1,7 +1,6 @@
 import unittest2 as unittest
 import unit_tests
-from colour_runner.runner import ColourTextTestRunner
 
 if __name__ == "__main__":
     all_tests = unittest.TestLoader().loadTestsFromModule(unit_tests)
-    ColourTextTestRunner(buffer=True, verbosity=2).run(all_tests)
+    unittest.TextTestRunner(verbosity=2).run(all_tests)
